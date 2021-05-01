14ff3fbfb4775aaeb1e96d4ace862108cd2adef4
Uzma-Noor <shaikuzma10@gmail.com>
2021-05-01 19:33:03 +0530

    changing local call back url

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ passport.deserializeUser(function(id, done) {
 const gStrategy = new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://pacific-beach-17776.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     // Make Strategy trust all proxy settings
     proxy: true
@@ -124,7 +124,7 @@ passport.use(gStrategy);
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "https://pacific-beach-17776.herokuapp.com/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log("Facebook profile" + profile)
